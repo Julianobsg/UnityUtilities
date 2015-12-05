dd0245297e89714f260f365177d84ab6620ca72d
Juliano Gomes <julianobsg@gmail.com>
2015-12-05 16:34:36 -0200

    add missing reference behaviors cleaner

---
+++ Editor/BehavioursCleaner.cs
@@ -0,0 +1,40 @@
+﻿namespace UnityUtilities.Editor
+{
+    using UnityEngine;
+    using UnityEditor;
+    using System;
+    using System.Collections.Generic;
+
+    public class BehavioursCleaner :  Editor
+	{	
+		[MenuItem("Edit/Clean Missing Behaviours")]
+        public static void CleanGameObjectsMissingBehaviours ()
+        {
+			foreach(GameObject go in FindObjectsOfType(typeof(GameObject)) as GameObject[])
+			{
+				CleanGameObject(go);	
+			}
+        }
+
+        private static void CleanGameObject(GameObject go)
+        {
+			Component[] components = go.GetComponents<Component>();
+			SerializedObject serializedObject = new SerializedObject(go);
+			removeComponentFromPoperty(components, serializedObject.FindProperty("m_Component"));
+			serializedObject.ApplyModifiedProperties();
+        }
+
+        private static void removeComponentFromPoperty(Component[] components, SerializedProperty property)
+        {
+            int removeComponents = 0;
+			for(int i = 0; i < components.Length; i++)
+			{
+				if(components[i] == null)
+				{
+					property.DeleteArrayElementAtIndex(i-removeComponents);
+					removeComponents++;
+				}
+			}
+        }
+    }
+}
